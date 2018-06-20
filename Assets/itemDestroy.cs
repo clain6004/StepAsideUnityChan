9278e77381c205ad875f1304758eaef5a8754d16
clain6004 <kirisame6004@gmail.com>
2018-06-20 21:02:49 +0900

    StepAsideUnityChanコミット itemDestroyが課題のスクリプトです。発展課題はItemGeneratorを使いました。

---
+++ Assets/itemDestroy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class itemDestroy : MonoBehaviour {
+
+     GameObject unitychan;
+
+   public float unitypos;
+
+    float itempos;
+
+    private float goalPos = 120;
+
+    // Use this for initialization
+    void Start () {
+
+        unitychan = GameObject.Find("unitychan");
+
+        itempos = this.transform.position.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        unitypos = unitychan.transform.position.z;
+
+        if (itempos+5 <= unitypos)
+        {
+
+            Destroy(this.gameObject);
+
+        }
+
+        if (itempos >= goalPos)
+        {
+
+            Destroy(this.gameObject);
+
+        }
+
+	}
+}
